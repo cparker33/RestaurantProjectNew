32c890030606d69e47d1cf36741f68098ece43d7
Caleb <caleb@theparker.me>
2017-04-07 10:15:07 -0700

    Adding Redux Files

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import store from '../store'
+import { getData } from '../api/bcbc';
+getData()
 
 export default React.createClass({
+
   render() {
+
     return (
+
       <h1>Hello world</h1>
+
     )
   }
 })
