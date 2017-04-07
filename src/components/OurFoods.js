3720be011915c95c90cc98cb1422817ab9456a76
Josephine Gabrielle Baltazar <jgsbaltazar@yahoo.com>
2017-04-07 10:52:15 -0700

    resolving merge for master

--- src/components/OurFoods.js
+++ src/components/OurFoods.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import data from './db.json'
-import '../api/Styles/OurFood.css';
+import '../api/Styles/OurFoods.css';
 
 class OurFood extends Component {
   render () {
 		return (
 		<div id="PhotoGallery">
     		
-    		{data.gallery.map(gallery=>(
-    			<img className="steaks" alt="" src={gallery.url} />
-    		))}
+    		
+    			
+    		
 
     	</div>
 		);
