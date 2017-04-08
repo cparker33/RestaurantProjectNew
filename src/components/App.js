b110f0c8324a075f907c854c736cac4386b24d59
Cheddarman <c.thecheesedelong@gmail.com>
2017-04-08 15:58:49 -0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
-import React from 'react'
 
-class  extends React.Component {
-  
+import React, { Component } from 'react';
+import store from '../store'
+import { getData } from '../api/bcbc'
+import '../assets/styles/App.css';
+import Menu from './Menu'
+
+
+getData()
+
+// Open Sans Font
+// Papyrus for Headers
+
+class App extends Component {
+
   render() {
+
     return (
-      <div>
-        
+
+
+      <div className="App">
+
+      <div id='AppHeader'>
+        <h1>Brown Chicken Brown Cow</h1>
       </div>
-    )
+
+        <div className='Main'>
+
+          
+          <Menu />
+          
+          
+
+        </div>
+
+      </div>
+    );
   }
 }
-export default 
+
+export default App;
